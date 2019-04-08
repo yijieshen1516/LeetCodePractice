500cd18d0613b499c6aa070f47c748b7673e5ece
Yijie Shen <shenyijie56@gmail.com>
2019-04-08 17:23:09 -0500

    change

--- python.py
+++ python.py
@@ -1,3 +1,42 @@
-import numpy as numpy
+
+class Solution(object):
+
+    def __init__(self,nums, target):
+        self.nums = nums
+        self.target = target
+
+    def searchRange(self):
+        n = len(self.nums)
+        left, right = -1, -1
+        l, r = 0, n-1
+        while l < r:
+            m = (l+r)//2
+            if self.nums[m] < self.target: 
+                l = m+1
+            else: 
+                r = m
+        if self.nums[l] != self.target: 
+            return -1, -1
+        left = l
+        l, r = left, n-1
+        while l < r:
+            m = (l+r)//2
+            if self.nums[m] == self.target: 
+                l = m
+            else: 
+                r = m-1
+        right = l
+        return left, right
+
+if __name__ == "__main__":
+ 
+    arr = [1, 2, 2, 2, 2, 3, 4, 7 ,8 ,8 ]
+    x = 5
+    c = Solution(arr, x)
+    left, right = c.searchRange()
+    print(left, right)
+
+
+
 
 

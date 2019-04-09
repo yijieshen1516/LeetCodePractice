3a4a812442a08d409c65555e73c964d6c486f502
Yijie Shen <shenyijie56@gmail.com>
2019-04-09 14:01:29 -0500

    changes

--- leetcodebinarysearch1.py
+++ leetcodebinarysearch1.py
@@ -13,7 +13,9 @@ class Solution(object):
             m = (l+r)//2
             if self.nums[m] < self.target: 
                 l = m+1
-            else: 
+            elif self.nums[m] > self.target:
+                r = m-1
+            else:
                 r = m
         if self.nums[l] != self.target: 
             return -1, -1
@@ -31,6 +33,7 @@ class Solution(object):
 if __name__ == "__main__":
  
     arr = [1, 2, 2, 2, 2, 3, 4, 7 ,8 ,8 ]
+    #arr = [1, 2, 3, 4]
     x = 2
     c = Solution(arr, x)
     left, right = c.searchRange()
---
+++ quick_sort.py
@@ -0,0 +1,35 @@
+def partition(array, low, up):
+
+    i = low +1
+    j = up
+    pivot = array[low]
+
+    while (i <= j):
+        while (array[i] < pivot) and (i<up):
+            i = i +1
+        while (array[j] > pivot):
+            j = j -1
+        if (i< j):
+            array[i], array[j] = array[j], array[i]
+            i = i+1
+            j = j-1
+        else:
+            i = i+1
+    array[low] = array[j]
+    array[j] = pivot
+
+    return j
+
+array = [48, 44, 19, 59, 72, 80, 42, 65, 82, 8, 95, 68]
+low = 0
+up = len(array) -1
+
+def quicksort(array, low, up):
+    if (low > up):
+        return
+    pivt_loc = partition(array, low, up)
+    quicksort(array, low, pivt_loc -1)
+    quicksort(array, pivt_loc +1, up)
+
+quicksort(array, low, up)
+print(array)
